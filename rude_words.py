53b8c401a13854b7f9d8fcbf28839f037b0a84be
Dedikova Anastasiia <adedikova@chegg.com>
2017-11-15 20:21:22 +0200

    rude-words-exercise_added

--- rude_words.py
+++ rude_words.py
@@ -1,131 +1,14 @@
-# -*- coding: utf-8 -*-
+rude_words = ['ass', 'bastard', 'crap', 'damn']
 
-# Списки
+message = """
+Since I needed to damn use them in a crap project (Humboldt Diglital ASS Library and Network),
+I am posting here a bastard list of English damn stop words,
+and below a PHP crap array containing these words
+"""
 
-# Заполните код преведенных ниже функций. Функция main() уже настроена
-# для вызова функций с несколькими различными параметрами,
-# и выводит 'OK' в случае, если вызов функции корректен.
-# Начальный код каждой функции содержит 'return'
-# и является просто заготовкой для вашего кода.
+message_list = message.split()
 
-
-# A. Начало и конец совпадают
-# Функция принимает в качестве аргумента список строк.
-# Необходимо вернуть количество строк,
-# длина которых составляет 2 символа и более, 
-# а первый и последний символы этих строк совпадают.
-# Примечание: в python нет оператора ++. Но += сработает.
-def match_ends(words):
-    res = 0
-    for x in words:
-        if len(x) >= 2:
-            if x[0:1] == x[-1]:
-                res += 1
-    return res
-
-
-# B. Начинающиеся с X в начале
-# Функция принимает в качестве аргумента список строк.
-# Необходимо вернуть отсортированный список строк, в котором:
-# сначала идет группа строк, начинающихся на 'x', затем все остальные.
-# Наример: из ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] получится
-# ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
-# Подсказка: это можно сделать при помощи склеивания 2х заранее отсортированных списков
-def front_x(words):
-    list_x = list()
-    list_other = list()
-    for x in words:
-        if x[0:1].upper() == 'X':
-            list_x.append(x)
-        else:
-            list_other.append(x)
-    list_x.sort()
-    list_other.sort()
-    return list_x + list_other
-
-
-# C. Сортировка по последнему числу
-# Дан спискок непустых списков. 
-# Нужно вернуть список, отсортированный по 
-# возрастанию последнего элемента каждого подсписка.
-# Например: из [[1, 7], [1, 3], [3, 4, 5], [2, 2]] получится
-# [[2, 2], [1, 3], [3, 4, 5], [1, 7]]
-# Подсказка: используйте параметр key= функции сортировки, 
-# чтобы получить последний элемент подсписка.
-# def sort_with_key(elem):  # sort list starting with 1st elm, esc
-#     return elem[-1]
-
-def get_last_element(elem):  # sort list starting with 1st elm, esc
-    return elem[-1]
-
-
-def sort_last(gotten_list):
-    gotten_list.sort(key=get_last_element)
-    return gotten_list
-
-
-# D. Удаление соседей
-# Дан список чисел.
-# Нужно вернуть список, где все соседние элементы
-# были бы сведены к одному элементу.
-# Таким образом, из [1, 2, 2, 3, 4, 4] получится [1, 2, 3, 4].
-def remove_adjacent(nums_list):
-    if len(nums_list) == 0:
-        return nums_list
-    else:
-        new_list = [nums_list[0]]
-        j = 0
-        i = 1
-        while i < len(nums_list):
-            if new_list[j] != nums_list[i]:
-                new_list.append(nums_list[i])
-                j += 1
-            i += 1
-        return new_list
-
-
-# Простая функция test() используется в main() для вывода
-# сравнения того, что возвращает с функция с тем, что она должна возвращать.
-def test(got, expected):
-    if got == expected:
-        prefix = ' OK '
-    else:
-        prefix = '  X '
-    print('%s Получено: %s | Ожидалось: %s' % (prefix, repr(got), repr(expected)))
-
-
-# Вызывает фунции выше с тестовыми параметрами.
-def main():
-    print('Начало и конец совпадают')
-    test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
-    test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
-    test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
-
-    print()
-    print('Начинающиеся с X в начале')
-    test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
-         ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
-    test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
-         ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
-    test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
-         ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
-
-    print()
-    print('Сортировка по последнему числу')
-    test(sort_last([[1, 3], [3, 2], [2, 1]]),
-         [[2, 1], [3, 2], [1, 3]])
-    test(sort_last([[2, 3], [1, 2], [3, 1]]),
-         [[3, 1], [1, 2], [2, 3]])
-    test(sort_last([[1, 7], [1, 6], [3, 4, 5], [2, 2]]),
-         [[2, 2], [3, 4, 5], [1, 6], [1, 7]])
-
-    print()
-    print('Удаление соседей')
-    test(remove_adjacent([1, 2, 2, 3]), [1, 2, 3])
-    test(remove_adjacent([2, 2, 3, 3, 3, 3]), [2, 3])
-    test(remove_adjacent([4, 5, 5, 4, 4]), [4, 5, 4])
-    test(remove_adjacent([]), [])
-
-
-if __name__ == '__main__':
-    main()
+for number, word in enumerate(message_list):
+    if word.lower() in rude_words:
+        message_list[number] = '*'
+print ' '.join(message_list)
